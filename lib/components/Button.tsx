ecaa45d4b13560373715191b02375a57a9ab4ab1
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-12-29 12:52:19 -0500

    Allow Button intent="none"

--- lib/components/Button.tsx
+++ lib/components/Button.tsx
@@ -1,5 +1,5 @@
 import { Button as HeadlessButton } from "@headlessui/react";
-import type { MouseEvent, PropsWithChildren } from "react";
+import type { KeyboardEvent, MouseEvent, PropsWithChildren } from "react";
 import type { Intent } from "../types";
 import { cn } from "../utils/cn";
 
@@ -7,13 +7,14 @@ export function Button({
   children,
   className,
   disabled = false,
-  intent = "none",
+  intent = "primary",
   ...rest
 }: PropsWithChildren<{
   className?: string | undefined;
   disabled?: boolean | undefined;
   intent?: Intent | undefined;
   onClick?: ((event: MouseEvent) => void) | undefined;
+  onKeyDown?: ((event: KeyboardEvent) => void) | undefined;
 }>) {
   return (
     <HeadlessButton
@@ -37,25 +38,28 @@ export function Button({
 function getClassNames(intent: Intent, disabled: boolean) {
   switch (intent) {
     case "danger": {
-      return cn("bg-red-400 text-red-800 focus:text-black", {
-        "hover:bg-red-500 hover:text-red-950 focus:text-black": !disabled
+      return cn("bg-red-400 text-red-950 focus:text-black", {
+        "hover:bg-red-500 focus:text-black": !disabled
       });
     }
     case "none": {
-      return cn("bg-emerald-400 text-emerald-800 focus:text-black", {
-        "hover:bg-emerald-500 hover:text-emerald-950 focus:text-black":
-          !disabled
+      return cn("bg-white/20 text-slate-300 focus:text-white", {
+        "hover:bg-white/10 focus:text-white": !disabled
       });
     }
-    case "success":
+    case "success": {
+      return cn("bg-emerald-400 text-emerald-950 focus:text-black", {
+        "hover:bg-emerald-500 focus:text-black": !disabled
+      });
+    }
     case "primary": {
-      return cn("bg-sky-400 text-sky-800 focus:text-black", {
-        "hover:bg-sky-500 hover:text-sky-950 focus:text-black": !disabled
+      return cn("bg-sky-400 text-sky-950 focus:text-black", {
+        "hover:bg-sky-500 focus:text-black": !disabled
       });
     }
     case "warning": {
-      return cn("bg-amber-400 text-amber-800 focus:text-black", {
-        "hover:bg-amber-500 hover:text-amber-950 focus:text-black": !disabled
+      return cn("bg-amber-400 text-amber-950 focus:text-black", {
+        "hover:bg-amber-500 focus:text-black": !disabled
       });
     }
   }
