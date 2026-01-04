e603ce7246b3893be212fb10319fae30d3fe0ed2
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-01-04 14:51:02 -0500

    OG image generation exports SVG and PNG files

--- src/components/og-image/DownloadableSvg.tsx
+++ src/components/og-image/DownloadableSvg.tsx
@@ -4,51 +4,65 @@ import { height, width } from "./constants";
 
 export function DownloadableSvg({ children }: PropsWithChildren) {
   return (
-    <Tooltip content="Click to download og.png">
+    <Tooltip content="Left-click to download og.png; right-click to download og.svg">
       <div
         className="cursor-pointer"
         onClick={async (event) => {
           event.preventDefault();
 
-          let current = event.target as HTMLElement | SVGAElement | null;
-          while (current) {
-            if (current.tagName === "svg") {
-              const svg = current as SVGElement;
-              const xmlSerializer = new XMLSerializer();
-              const svgString = xmlSerializer.serializeToString(svg);
-              console.log("svg:\n" + svgString);
+          const dataUrl = getSvgDataUrl(event.target);
 
-              const image = new Image();
+          const image = new Image();
 
-              await new Promise((resolve, reject) => {
-                image.onload = resolve;
-                image.onerror = reject;
-                image.src = `data:image/svg+xml;base64,${btoa(svgString)}`;
-              });
+          await new Promise((resolve, reject) => {
+            image.onload = resolve;
+            image.onerror = reject;
+            image.src = dataUrl;
+          });
 
-              const canvas = document.createElement("canvas");
-              canvas.width = width;
-              canvas.height = height;
+          const canvas = document.createElement("canvas");
+          canvas.width = width;
+          canvas.height = height;
 
-              const context = canvas.getContext(
-                "2d"
-              ) as CanvasRenderingContext2D;
-              context.drawImage(image, 0, 0, width, height);
+          const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+          context.drawImage(image, 0, 0, width, height);
 
-              const anchor = document.createElement("a");
-              anchor.download = "og.png";
-              anchor.href = canvas.toDataURL("image/png");
-              anchor.click();
+          downloadAsFile("og.png", canvas.toDataURL("image/png"));
+        }}
+        onContextMenu={async (event) => {
+          event.preventDefault();
 
-              return;
-            }
+          const dataUrl = getSvgDataUrl(event.target);
 
-            current = current.parentElement;
-          }
+          downloadAsFile("og.svg", dataUrl);
         }}
       >
         {children}
       </div>
     </Tooltip>
   );
+}
+
+function downloadAsFile(fileName: string, data: string) {
+  const anchor = document.createElement("a");
+  anchor.download = fileName;
+  anchor.href = data;
+  anchor.click();
+}
+
+function getSvgDataUrl(target: EventTarget): string {
+  let current = target as HTMLElement | SVGElement | null;
+  while (current) {
+    if (current.tagName === "svg") {
+      const svg = current as SVGElement;
+      const xmlSerializer = new XMLSerializer();
+      const htmlString = xmlSerializer.serializeToString(svg);
+
+      return `data:image/svg+xml;base64,${btoa(htmlString)}`;
+    }
+
+    current = current.parentElement;
+  }
+
+  throw Error("SVG Element not found");
 }
